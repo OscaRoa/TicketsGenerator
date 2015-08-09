64d1f7d8accb4c3c613f511d0c394d5a3dfa439a
Oscar Roa <oshardy3@gmail.com>
2015-08-08 23:40:49 -0500

    index.js: Nuevo módulo agregado.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express');
+var parser = require('body-parser');
 var app = express();
 var puerto = 3000;
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true}));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/tickets', function (req, res) {
-  res.sendFile(__dirname + '/ticket.html')
+  var nombre = req.body;
+  console.log(nombre);
 });
 
 app.listen(puerto, function () {
